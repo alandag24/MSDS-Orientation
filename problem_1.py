d912c8e8fcce35f06b710007a6b20500f68aa0b5
alandag24 <alandag24@gmail.com>
2024-06-14 14:32:10 -0400

    Solved Problem 1
    Solved Problem 1 from Euler's

--- problem_1.py
+++ problem_1.py
@@ -1,6 +1,19 @@
-print("hello world")
 
-print("test")
+# technical orientation problem 1 euler's
 
 
-print("update")
+import pandas as pd
+import numpy as num
+import matplotlib as plt
+
+sum = 0
+
+for num in range(1,1000):
+    if num % 3 == 0:
+        sum += num
+    elif num % 5 == 0:
+        sum += num
+    num += 1
+print(sum)
+
+
